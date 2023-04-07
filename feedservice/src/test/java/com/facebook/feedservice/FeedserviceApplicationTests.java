64a9316784b9b9b2281afb920f90f4e771448228
reljod <oretareljod@gmail.com>
2023-04-07 09:24:37 +0800

    Add initial feedservice

---
+++ feedservice/src/test/java/com/facebook/feedservice/FeedserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facebook.feedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
